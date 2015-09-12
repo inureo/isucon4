3b0f19acb714c36a8b9376213bd9de54c138c0a9
inureo <ryo_tezuka@r.recruit.co.jp>
2015-09-12 15:22:57 +0900

    new relic 導入

--- ruby/app.rb
+++ ruby/app.rb
@@ -4,6 +4,9 @@ require 'mysql2-cs-bind'
 require 'rack-flash'
 require 'json'
 require 'redis'
+require 'newrelic_rpm'
+
+NewRelic::Agent.after_fork(:force_reconnect => true)
 
 module Isucon4
   class App < Sinatra::Base
